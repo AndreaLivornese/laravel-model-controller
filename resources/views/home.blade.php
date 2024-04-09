a4288118f9f4b33c886c039df6837bad43dde3a3
andrealivornese <andrealivornese@gmail.com>
2024-04-09 16:12:32 +0200

    creazione delle card e fine eserczio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,33 @@
 
 @section('content')
 
-<ul>
-    @foreach ($movies as $movie)
+
+<h1 class="text-center mb-5">   
+ I miei film Preferiti
+</h1>
+
+
+<div class="container">
     
-    <li>
-            {{$movie->title}}
-    </li>
-    @endforeach
-</ul>
+    <div class="row row-cols-3 row-gap-3">
+        
+        @foreach ($movies as $movie)
+            
+        <div class="col">
+            <div class="card inner-shadow">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie->original_title}}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie->title}}</h6>
+                    <p class="card-text">{{$movie->nationality}}</p>
+                    <p class="card-text">{{$movie->vote}}</p>
+                    <p class="card-text">{{$movie->date}}</p>
+                </div>
+            </div>
+        </div>
+        
+        @endforeach
+    </div>
+    
+    </div>
     
 @endsection
