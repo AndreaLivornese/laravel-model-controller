604eb4614abc3488becd9a511a7bc138c2266238
andrealivornese <andrealivornese@gmail.com>
2024-04-09 15:54:29 +0200

    estrapolazione dei dati in tabella con l'utilizzo del model e stampa in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts/app')
+
+@section('content')
+
+<ul>
+    @foreach ($movies as $movie)
+    
+    <li>
+            {{$movie->title}}
+    </li>
+    @endforeach
+</ul>
+    
+@endsection
